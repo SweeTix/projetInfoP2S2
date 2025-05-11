59acbaf52054679cef48ec4a3ce40381b161af6c
Tom_ma95 <167717246+Drackfish@users.noreply.github.com>
2025-05-11 17:28:14 +0200

    Add files via upload

--- PHP/Verification.php
+++ PHP/Verification.php
@@ -16,16 +16,21 @@
             foreach($lignes as $ligne){
                 $utilisateur = explode(";", $ligne);
 
-                if($email === $utilisateur[3]){
+                if($email === $utilisateur[4]){
 
-                    if($mdp === $utilisateur[4]){
+                    if($mdp === $utilisateur[5]){
+                        if($utilisateur[5] === "admin"){
+                            header("location: administrateur.php");
+                            exit();
+                        }
                         $_SESSION['user_name'] = $utilisateur[0];
                         $_SESSION['user_firstname'] = $utilisateur[1];
-                        $_SESSION['user_address'] = $utilisateur[2];
-                        $_SESSION['user_email'] = $utilisateur[3];
-                        $_SESSION['user_password'] = $utilisateur[4];
+                        $_SESSION['user_birth'] = $utilisateur[2];
+                        $_SESSION['user_address'] = $utilisateur[3];
+                        $_SESSION['user_email'] = $utilisateur[4];
+                        $_SESSION['user_password'] = $utilisateur[5];
+                        $_SESSION['user_statut'] = $utilisateur[6];
                         header("Location: pageprofil.php");
-                        
                         exit();
                     }
                     header("Location: connexion.php?erreur=mdp_incorrect");

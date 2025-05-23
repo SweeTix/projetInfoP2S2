634b4c9cdd60d43ea370116654f97aa5862e8501
fredIcode5 <168578739+fredIcode5@users.noreply.github.com>
2025-05-23 15:24:53 +0200

    Add files via upload

---
+++ PHP/annonce.php
@@ -0,0 +1,14 @@
+
+
+<div class="annonce">
+                    <button><a href="attribution_voyage2.php?destination=<?php echo urlencode($destination); ?>"><img src="montagne2.jpg" alt="Voyage 1"></a></button>
+                    <p><?php echo $destination ?></p>
+                    <ul class="aligné">
+                        <li><div class="tag"><?php echo $tag1 ?></div></li>
+                        <li><div class="tag"><?php echo $tag2 ?></div></li>
+                     </ul>
+                     <ul class="aligné">
+                        <li><div class="tag"><?php echo $tag3 ?></div></li>
+                        <li><div class="tag"><?php echo $tag4 ?></div><lI>
+                     </ul>             
+                </div>
